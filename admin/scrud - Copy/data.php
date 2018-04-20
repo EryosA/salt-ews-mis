9fd9d391a3bac07ba7ad5012391062dfce7f62f4
EryosA <36581119+EryosA@users.noreply.github.com>
2018-04-20 15:01:23 +0800

    Edited scrud/data.php

--- admin/scrud - Copy/data.php
+++ admin/scrud - Copy/data.php
@@ -3,19 +3,18 @@
 $db_server   = 'localhost';
 $db_username = 'root';
 $db_password = '';
-//$db_name     = 'it_companies';
-$db_name     = 'salt_db';
+$db_name     = 'it_companies';
 
 // Get job (and id)
 $job = '';
 $id  = '';
 if (isset($_GET['job'])){
   $job = $_GET['job'];
-  if ($job == 'get_rental_pool_registration_records' ||
-      $job == 'get_rental_pool'   ||
-      $job == 'add_rental_pool'   ||
-      $job == 'edit_rental_pool'  ||
-      $job == 'delete_rental_pool'){
+  if ($job == 'get_companies' ||
+      $job == 'get_company'   ||
+      $job == 'add_company'   ||
+      $job == 'edit_company'  ||
+      $job == 'delete_company'){
     if (isset($_GET['id'])){
       $id = $_GET['id'];
       if (!is_numeric($id)){
@@ -42,10 +41,10 @@ if ($job != ''){
   }
   
   // Execute job
-  if ($job == 'get_rental_pool_registration_records'){
+  if ($job == 'get_companies'){
     
-    // Get rental pool records
-    $query = "SELECT * FROM 'rental_pool_registration_records' ORDER BY 'DateTime'";
+    // Get companies
+    $query = "SELECT * FROM it_companies ORDER BY rank";
     $query = mysqli_query($db_connection, $query);
     if (!$query){
       $result  = 'error';
@@ -53,33 +52,33 @@ if ($job != ''){
     } else {
       $result  = 'success';
       $message = 'query success';
-      while ($rental_pool = mysqli_fetch_array($query)){
+      while ($company = mysqli_fetch_array($query)){
         $functions  = '<div class="function_buttons"><ul>';
-        $functions .= '<li class="function_edit"><a data-id="'   . $rental_pool['RPRecordID'] . '" data-name="' . $rental_pool['Model'] . '"><span>Edit</span></a></li>';
-        $functions .= '<li class="function_delete"><a data-id="' . $rental_pool['RPRecordID'] . '" data-name="' . $rental_pool['Model'] . '"><span>Delete</span></a></li>';
+        $functions .= '<li class="function_edit"><a data-id="'   . $company['company_id'] . '" data-name="' . $company['company_name'] . '"><span>Edit</span></a></li>';
+        $functions .= '<li class="function_delete"><a data-id="' . $company['company_id'] . '" data-name="' . $company['company_name'] . '"><span>Delete</span></a></li>';
         $functions .= '</ul></div>';
         $mysql_data[] = array(
-          "rank"          => $rental_pool['rank'],
-          "Model"  => $rental_pool['Model'],
-          "industries"    => $rental_pool['industries'],
-          "revenue"       => '$ ' . $rental_pool['revenue'],
-          "fiscal_year"   => $rental_pool['fiscal_year'],
-          "employees"     => number_format($rental_pool['employees'], 0, '.', ','),
-          "market_cap"    => '$ ' . $rental_pool['market_cap'],
-          "headquarters"  => $rental_pool['headquarters'],
+          "rank"          => $company['rank'],
+          "company_name"  => $company['company_name'],
+          "industries"    => $company['industries'],
+          "revenue"       => '$ ' . $company['revenue'],
+          "fiscal_year"   => $company['fiscal_year'],
+          "employees"     => number_format($company['employees'], 0, '.', ','),
+          "market_cap"    => '$ ' . $company['market_cap'],
+          "headquarters"  => $company['headquarters'],
           "functions"     => $functions
         );
       }
     }
     
-  } elseif ($job == 'get_rental_pool'){
+  } elseif ($job == 'get_company'){
     
     // Get company
     if ($id == ''){
       $result  = 'error';
       $message = 'id missing';
     } else {
-      $query = "SELECT * FROM 'rental_pool_registration_records' WHERE RPRecordID = '" . mysqli_real_escape_string($db_connection, $id) . "'";
+      $query = "SELECT * FROM it_companies WHERE company_id = '" . mysqli_real_escape_string($db_connection, $id) . "'";
       $query = mysqli_query($db_connection, $query);
       if (!$query){
         $result  = 'error';
@@ -87,27 +86,27 @@ if ($job != ''){
       } else {
         $result  = 'success';
         $message = 'query success';
-        while ($rental_pool = mysqli_fetch_array($query)){
+        while ($company = mysqli_fetch_array($query)){
           $mysql_data[] = array(
-            "rank"          => $rental_pool['rank'],
-            "Model"  => $rental_pool['Model'],
-            "industries"    => $rental_pool['industries'],
-            "revenue"       => $rental_pool['revenue'],
-            "fiscal_year"   => $rental_pool['fiscal_year'],
-            "employees"     => $rental_pool['employees'],
-            "market_cap"    => $rental_pool['market_cap'],
-            "headquarters"  => $rental_pool['headquarters']
+            "rank"          => $company['rank'],
+            "company_name"  => $company['company_name'],
+            "industries"    => $company['industries'],
+            "revenue"       => $company['revenue'],
+            "fiscal_year"   => $company['fiscal_year'],
+            "employees"     => $company['employees'],
+            "market_cap"    => $company['market_cap'],
+            "headquarters"  => $company['headquarters']
           );
         }
       }
     }
   
-  } elseif ($job == 'add_rental_pool'){
+  } elseif ($job == 'add_company'){
     
     // Add company
     $query = "INSERT INTO it_companies SET ";
     if (isset($_GET['rank']))         { $query .= "rank         = '" . mysqli_real_escape_string($db_connection, $_GET['rank'])         . "', "; }
-    if (isset($_GET['Model'])) { $query .= "company_name = '" . mysqli_real_escape_string($db_connection, $_GET['Model']) . "', "; }
+    if (isset($_GET['company_name'])) { $query .= "company_name = '" . mysqli_real_escape_string($db_connection, $_GET['company_name']) . "', "; }
     if (isset($_GET['industries']))   { $query .= "industries   = '" . mysqli_real_escape_string($db_connection, $_GET['industries'])   . "', "; }
     if (isset($_GET['revenue']))      { $query .= "revenue      = '" . mysqli_real_escape_string($db_connection, $_GET['revenue'])      . "', "; }
     if (isset($_GET['fiscal_year']))  { $query .= "fiscal_year  = '" . mysqli_real_escape_string($db_connection, $_GET['fiscal_year'])  . "', "; }
@@ -123,7 +122,7 @@ if ($job != ''){
       $message = 'query success';
     }
   
-  } elseif ($job == 'edit_rental_pool'){
+  } elseif ($job == 'edit_company'){
     
     // Edit company
     if ($id == ''){
@@ -132,7 +131,7 @@ if ($job != ''){
     } else {
       $query = "UPDATE it_companies SET ";
       if (isset($_GET['rank']))         { $query .= "rank         = '" . mysqli_real_escape_string($db_connection, $_GET['rank'])         . "', "; }
-      if (isset($_GET['Model'])) { $query .= "company_name = '" . mysqli_real_escape_string($db_connection, $_GET['Model']) . "', "; }
+      if (isset($_GET['company_name'])) { $query .= "company_name = '" . mysqli_real_escape_string($db_connection, $_GET['company_name']) . "', "; }
       if (isset($_GET['industries']))   { $query .= "industries   = '" . mysqli_real_escape_string($db_connection, $_GET['industries'])   . "', "; }
       if (isset($_GET['revenue']))      { $query .= "revenue      = '" . mysqli_real_escape_string($db_connection, $_GET['revenue'])      . "', "; }
       if (isset($_GET['fiscal_year']))  { $query .= "fiscal_year  = '" . mysqli_real_escape_string($db_connection, $_GET['fiscal_year'])  . "', "; }
@@ -150,7 +149,7 @@ if ($job != ''){
       }
     }
     
-  } elseif ($job == 'delete_rental_pool'){
+  } elseif ($job == 'delete_company'){
   
     // Delete company
     if ($id == ''){
